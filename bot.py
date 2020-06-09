156d8c9c4d7c3f965e1848b8d0d85d0ae816a8da
Solokola <66673039+Solokola@users.noreply.github.com>
2020-06-09 13:17:17 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,14 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # -*- coding: utf-8 -*-
+import config
+import telebot
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.infinity_polling()
---
+++ config.py
@@ -0,0 +1,5 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # -*- coding: utf-8 -*-
+# Токен ненастоящий :) Подставьте свой
+token = '1266530398:AAGfrT2NUYU2wkSe-0Qe-KJbaYAkxdbaQqk'
